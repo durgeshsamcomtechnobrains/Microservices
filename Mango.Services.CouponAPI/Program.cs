34c0a92a4e07f7d245b0187b4b25f82cc1421fc2
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-09-13 14:50:45 +0530

    Add project files.

---
+++ Mango.Services.CouponAPI/Program.cs
@@ -0,0 +1,46 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefalutConnectionString"));
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+AddMigration();
+app.Run();
+
+void AddMigration()
+{
+    using(var scoped = app.Services.CreateScope())
+    {
+        var _db = scoped.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
