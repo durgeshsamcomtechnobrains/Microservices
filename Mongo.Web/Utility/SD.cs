9aca3adb58266643a43835763a8898b9da2d27c9
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-09-19 12:12:23 +0530

    Create Request and Response DTO

---
+++ Mongo.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mongo.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST, 
+            PUT,
+            DELETE,
+        }
+    }
+}
