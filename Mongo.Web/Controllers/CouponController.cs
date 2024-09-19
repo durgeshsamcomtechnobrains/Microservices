c14d8e15f4ad0f9f6d2a3ea5e1952eb86da5b861
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-09-19 16:00:34 +0530

    Bootswatch Theme and Bootstrap Icons and Coupon Controller

---
+++ Mongo.Web/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mongo.Web.Models;
+using Mongo.Web.Services.IService;
+using Newtonsoft.Json;
+
+namespace Mongo.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
