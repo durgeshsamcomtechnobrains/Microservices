80a42075ff5319e3ca3a344f0ec19b1bdfa18732
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-09-20 11:08:20 +0530

    AddDTO for Ath

---
+++ Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
